f85be10eaddb8f0ad6c7a1ec6355386277b18b81
john <you@example.com>
2024-07-18 14:18:28 +0530

    engines

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,4 +3,13 @@ const validator = require("validator")
 
 exports.registerAdmin == asyncHandler((req, res)=> {
     const {name, email, password}= req.body
+    if(validator.isEmpty(name)){
+        return res.status(400).json({message:"Name Is Required"})
+    }
+    if(validator.isEmpty(email)){
+        return res.status(400).json({message:"Email Is Required"})
+    }
+    if(validator.isEmpty(passsword)){
+        return res.status(400).json({message:"Passsword Is Required"})
+    }
 })
