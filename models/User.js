f2ae7263e9931a7884d6484a27f6e0b763f04fc3
john <you@example.com>
2024-07-18 14:07:25 +0530

    initial commit

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name:{type:String, required:true},
+    email:{type:String, required:true},
+    avatar:{type:String, default:"https://res.cloudinary.com/dpc5d15ci/image/upload/v1721291386/m2i8d3i8N4d3N4K9_rzvcsp.png"},
+    password:{type:String, required:true},
+    verified:{type:Boolean, default:false},
+    isDeleted:{type:Boolean, default:false},
+    mobile:{type:Number, required:true},
+    active:{type:Boolean, default:true},
+    code:{type:String},
+},{timestamps:true})
+
+module.exports = mongoose.model("user", userSchema)
