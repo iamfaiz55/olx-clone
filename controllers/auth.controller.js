25dfff7204858d2dbd491f352a655c695425192d
john <you@example.com>
2024-07-18 14:14:28 +0530

    dist add

---
+++ controllers/auth.controller.js
@@ -0,0 +1,6 @@
+const asyncHandler = require("express-async-handler")
+const validator = require("validator")
+
+exports.registerAdmin == asyncHandler((req, res)=> {
+    const {name, email, password}= req.body
+})
